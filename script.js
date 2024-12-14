80b04ccce3cd0adadcf878a5ba40a2b9106cd88d
edflorese <edflorese@uce.edu.ec>
2024-12-13 19:04:55 -0500

    feat: :sparkles: New excersices

--- script.js
+++ script.js
@@ -15,8 +15,25 @@ const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
-const monsterHealthText = document.querySelector("#monsterHealth ");
+const monsterHealthText = document.querySelector("#monsterHealth");
+const locations = [];
+const cat = {
+  name:"Whiskers",
+  "Number of legs":4
+};
 
+console.log(cat);
+
+function goTown() {
+  button1.innerText = "Go to store";
+  button2.innerText = "Go to cave";
+  button3.innerText = "Fight dragon";
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+  text.innerText =
+    'You are in the town square. You see a sign that says "Store".';
+}
 function goStore() {
   button1.innerText = "Buy 10 health (10 gold)";
   button2.innerText = "Buy weapon (30 gold)";
@@ -24,6 +41,7 @@ function goStore() {
   button1.onclick = buyHealth;
   button2.onclick = buyWeapon;
   button3.onclick = goTown;
+  text.innerText = "You enter the store.";
 }
 function goCave() {
   console.log("Going to cave.");
@@ -31,6 +49,9 @@ function goCave() {
 function fightDragon() {
   console.log("Fighting dragon.");
 }
+function buyHealth() {}
+function buyWeapon() {}
+function update(location) {}
 
 //initialize buttons
 button1.onclick = goStore;
