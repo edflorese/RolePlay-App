995e959ae6ffce2904f6d030e77cde16fab95d6f
edflorese <edflorese@uce.edu.ec>
2024-12-30 17:57:43 -0500

    feat: :sparkles: Sell weapond funtion created

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ function buyHealth() {
   }
 }
 function buyWeapon() {
-  if (currentWeaponIndex < weapons.length-1) {
+  if (currentWeaponIndex < weapons.length - 1) {
     if (gold >= 30) {
       gold -= 30;
       currentWeaponIndex++;
@@ -107,8 +107,14 @@ function buyWeapon() {
     } else {
       text.innerText = "You do not have enough gold to buy a weapon.";
     }
-  }else{
-    text.innerText="You already have the most powerful weapon!"
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
+  }
+}
+function sellWeapon() {
+  if (inventory.length > 1) {
   }
 }
 function update(location) {
